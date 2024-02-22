57e50e115798c5c7c36a7038fe04b260265eaa3a
satrik <dev@petrik.de>
2024-02-22 14:04:34 +0100

    test

--- testing.rb
+++ testing.rb
@@ -7,14 +7,6 @@ cask "testing" do
   desc "testing desc"
   homepage "https://github.com/satrik/testing"
 
-  livecheck do
-    url "https://github.com/satrik/testing/releases/download/#{version}/testing.zip"
-    regex(/testing.zip/i)
-    strategy :page_match do |page, regex|
-      page.scan(regex).map { |match| "#{match[0]},#{match[1]}" }
-    end
-  end
-
   depends_on macos: ">= :mojave"
 
   app "testing.app"
