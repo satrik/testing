6add87d5df64b9105573e08fbf4cee5e86443ae7
satrik <dev@petrik.de>
2024-02-22 11:53:08 +0100

    test

--- testing.rb
+++ testing.rb
@@ -2,7 +2,7 @@ class Testing < Formula
   desc "testing desc"
   homepage "https://github.com/satrik/testing"
 
-  depends_on macos: ":mojave"
+  depends_on macos: ":big_sur"
   
   url "https://github.com/satrik/testing/releases/download/0.1/testing.zip"
 
